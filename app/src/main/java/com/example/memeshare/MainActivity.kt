e80e8a5cff015343abe9090bf6e4981dcdea895b
unknown <prateekkasaudhan123@gmail.com>
2022-05-14 11:10:49 +0530

    --amaend

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -2,18 +2,12 @@ package com.example.memeshare
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
